9708e9b05c4b5c4a18fc5cef8c79b2f52e81ca2a
Cysthen <cysthen@hotmail.com>
2025-02-07 18:28:01 -0500

    Add files via upload

---
+++ Sounds/ComixSounds.lua
@@ -0,0 +1,74 @@
+-- Comix Sounds 
+ComixSounds = {};
+ComixSounds[1] = "Comix1.wav"
+ComixSounds[2] = "Comix2.wav"
+ComixSounds[3] = "Comix3.wav"
+ComixSounds[4] = "Comix4.wav"
+ComixSounds[5] = "punch1.wav"
+ComixSounds[6] = "punch2.wav"
+ComixSounds[7] = "punch3.wav"
+ComixSounds[8] = "punch4.wav"
+ComixSounds[9] = "Kung Fu Shout.wav"
+ComixSounds[10] = "yourdaysareover.wav"
+
+-- Comix Special Sounds --
+ComixSpecialSounds = {}
+ComixSpecialSounds[1] = "SpecialSounds\\Battleshout.wav"
+ComixSpecialSounds[2] = "SpecialSounds\\DemoShout.wav"
+ComixSpecialSounds[3] = "SpecialSounds\\DemoShout2.wav"
+ComixSpecialSounds[4] = "SpecialSounds\\vegeta.wav"
+ComixSpecialSounds[5] = "SpecialSounds\\dr_evil.wav"
+ComixSpecialSounds[6] = "SpecialSounds\\allyourbase.wav"
+ComixSpecialSounds[7] = "SpecialSounds\\realworld.wav"
+ComixSpecialSounds[8] = "SpecialSounds\\MARIO.wav"
+ComixSpecialSounds[9] = "SpecialSounds\\welldone.wav"
+ComixSpecialSounds[10] = "SpecialSounds\\pyroblast.wav"
+ComixSpecialSounds[11] = "SpecialSounds\\Space Balls - Ludicrous Speed.wav"
+ComixSpecialSounds[12] = "SpecialSounds\\finishim.wav"
+ComixSpecialSounds[13] = "SpecialSounds\\bam.wav"
+ComixSpecialSounds[14] = "SpecialSounds\\TYM.wav"
+ComixSpecialSounds[15] = "SpecialSounds\\air horn sad violin.wav"
+ComixSpecialSounds[16] = "SpecialSounds\\momentofglory.wav"
+ComixSpecialSounds[17] = "SpecialSounds\\dkdeath.wav"
+ComixSpecialSounds[18] = "SpecialSounds\\victor.wav"
+ComixSpecialSounds[19] = "SpecialSounds\\nogodno.wav"
+ComixSpecialSounds[20] = "SpecialSounds\\congratulations1.wav"
+
+-- Comix KillCount Sounds --
+ComixKillCountSounds = {}
+ComixKillCountSounds[1] =  "ComixKillCounSounds\\killingspree.wav"
+ComixKillCountSounds[2] =  "ComixKillCounSounds\\rampage.wav"
+ComixKillCountSounds[3] =  "ComixKillCounSounds\\multikill.wav"
+ComixKillCountSounds[4] =  "ComixKillCounSounds\\unstoppable.wav"
+ComixKillCountSounds[5] =  "ComixKillCounSounds\\ultrakill.wav"
+ComixKillCountSounds[6] =  "ComixKillCounSounds\\dominating.wav"
+ComixKillCountSounds[7] =  "ComixKillCounSounds\\godlike.wav"
+ComixKillCountSounds[8] =  "ComixKillCounSounds\\ludicrouskill.wav"
+ComixKillCountSounds[9] =  "ComixKillCounSounds\\monsterkill.wav"
+
+-- Comix Zone Sounds --
+ComixZoneSounds = {}
+ComixZoneSounds[1] = "ZoneSounds\\zone1 illuminati.wav"
+ComixZoneSounds[2] = "ZoneSounds\\zone2 metroid.wav"
+ComixZoneSounds[3] = "ZoneSounds\\zone3 tristram.wav"
+ComixZoneSounds[4] = "ZoneSounds\\zone4 krabby.wav"
+ComixZoneSounds[5] = "ZoneSounds\\zone5 krabby.wav"
+ComixZoneSounds[6] = "ZoneSounds\\zone6 radioshack.wav"
+ComixZoneSounds[7] = "ZoneSounds\\zone7 batman.wav"
+ComixZoneSounds[8] = "ZoneSounds\\zone8 yoo.wav"
+
+-- Comix MCombat Sounds --
+ComixOneHitSounds = {}
+ComixOneHitSounds[1] = "OneHitSounds\\brutality.wav"
+ComixOneHitSounds[2] = "OneHitSounds\\fatality.wav"
+ComixOneHitSounds[3] = "OneHitSounds\\supurb.wav"
+ComixOneHitSounds[4] = "OneHitSounds\\outstanding.wav"
+ComixOneHitSounds[5] = "OneHitSounds\\excelent.wav"
+
+-- Comix Healing Sounds --
+ComixHealingSounds = {}
+ComixHealingSounds[1] = "Healing\\toasty.wav"
+ComixHealingSounds[2] = "Healing\\surprise motherfucker.wav"
+ComixHealingSounds[3] = "Healing\\smoke weed everyday.wav"
+ComixHealingSounds[4] = "Healing\\alleluia.wav"
+ComixHealingSounds[5] = "Healing\\airporn.wav"
